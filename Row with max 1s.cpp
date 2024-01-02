6d6fa33907fff91ec2fbd3f35deb28b318ae2ba2
Conan0122 <109999045+Conan0122@users.noreply.github.com>
2024-01-03 00:28:56 +0530

    Update Row with max 1s.cpp
    Added another method using TWO POINTER approach.

--- Row with max 1s.cpp
+++ Row with max 1s.cpp
@@ -1,6 +1,5 @@
-// T.C = O(n^2)
+// T.C = O(n*m)
 // S.C = O(1)
-
 // Brute force approach (Accepted)
 class Solution{
 public:
@@ -19,6 +18,32 @@ public:
 	            }
 	        }
 	    }   
+	    return ans;
+	}
+};
+
+// T.C = O(n+m)
+// S.C = O(1)
+// Two pointer approach (Accepted)
+// This approach simple- Keep one pointer(i) for iterating row and other pointer(j) for coloumn.
+// Start i pointer from arr[0][0] till i< n and j pointer from arr[0][m-1] till j <= 0.
+// Now keep decrement j untill you get 0 and keep updating i as well. The moment you get 0, increment i and check if it equals to 1.
+// Keep incrementing i untill we get another 1.
+// return i as the answer.
+class Solution{
+public:
+	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+	    int ans=-1, i=0, j=m-1;
+	    
+	    while (i<n && j>=0){
+	        if (arr[i][j] == 1){
+	            j--;
+	            ans = i;
+	        }
+	        else {
+	            i++;
+	        }
+	    }
 	    return ans;
 	}
 };
