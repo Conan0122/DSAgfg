c3bc35b246d97176e621160ee7510be78b0c240c
Conan0122 <109999045+Conan0122@users.noreply.github.com>
2024-01-05 23:59:36 +0530

    04 Jan 2024- Medium

--- Find element occuring once when all other are present thrice.cpp
+++ Find element occuring once when all other are present thrice.cpp
@@ -20,4 +20,36 @@ class Solution {
     }
 };
 
-// Another way of solving this is using bit manipulation
+// Another way of solving this is using bit manipulation.
+// T.C = O(n)
+// S.C = O(1)
+class Solution {
+  public:
+    int singleElement(int arr[] ,int N) {
+        int res=0;
+        // Get the sum of every element at eaach bit position.
+        // Example: 3,6,3,3 => 3 in binary = 011  and 6 in binary = 110,
+        // At first position from right count the set bits in each element. In this case we get following:-
+        // 0  1  1
+        // 1  1  0
+        // 0  1  1
+        // 0  1  1
+        //---------
+        // 1  4  3
+        for (int i=0; i<32; i++){
+            int sum = 0;
+            for (int j=0; j<N; j++){
+                // Now to get the if current bit position is set bit or not, we have to AND currrent element with current bit position.
+                // We know our current element is arr[j] and current bit position is (1<<i).
+                if (arr[j]&(1<<i)){
+                    sum++;
+                }
+            }
+            // Now we know that the position with set bits not a multiple of 3 is the set bits of unique number(single number).
+            if (sum%3 == 1){
+                res = res | (1<<i);
+            }
+        }
+        return res;
+    }
+};
