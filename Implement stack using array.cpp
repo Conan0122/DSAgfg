235c48ebb4947287930880c8538a44644bea8f86
Conan0122 <109999045+Conan0122@users.noreply.github.com>
2024-01-30 23:59:32 +0530

    30 Jan 2024- Basic

---
+++ Implement stack using array.cpp
@@ -0,0 +1,15 @@
+// First problem on stacks DS.
+void MyStack :: push(int x)
+{
+    top++;
+    arr[top] = x;
+}
+
+//Function to remove an item from top of the stack.
+int MyStack :: pop()
+{
+    if (top == -1) return -1;
+    int temp = top;
+    top--;
+    return arr[temp];
+}
