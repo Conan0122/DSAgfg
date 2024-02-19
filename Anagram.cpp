015616086e2b263895ca43c845946c0a993cf46d
Conan0122 <109999045+Conan0122@users.noreply.github.com>
2024-02-20 01:17:44 +0530

    19 Feb 2024- Easy

--- Anagram.cpp
+++ Anagram.cpp
@@ -22,3 +22,28 @@ bool isAnagram(string a, string b){
         
         return true;
     }
+
+///////////////////////////////////////////////////////////////
+// T.C = O(n)
+// S.C = O(1)
+// With the help of ascii values.
+// Create an array of size 26(english alphabets) with 0 in each index.
+// IF both strings aren't of same size then return false.
+bool isAnagram(string a, string b){
+        vector<int> v(26,0);
+        if (a.size() != b.size()) return false;
+        // Now first increment the element in a[i]-'a' index.
+        // if a[i] = 'g' then we increment the index 'g' - 'a' which will be (ascii of 'g')-(ascii of 'a') i.e. 103-97 = 6.
+        // So we increment index 6 by one.
+        // At the same time we will decrement the element of b[i]-'a' index.
+        for (int i=0; i<a.size(); i++){
+            v[a[i] - 'a']++;
+            v[b[i] - 'a']--;
+        }
+        // Now check all the index of an array,
+        // IF we see any element either -ve or +ve (other than 0) then we know the strings weren't anagram.
+        for (int i=0; i<26; i++){
+            if (v[i]!=0) return false;
+        }
+        return true;
+}
