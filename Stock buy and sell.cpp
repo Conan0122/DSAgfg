57ec700c7d8d2876f173959ef8f148c9b7c5d622
Conan0122 <109999045+Conan0122@users.noreply.github.com>
2024-07-14 03:19:29 +0530

    14 July 2024- Medium

---
+++ Stock buy and sell.cpp
@@ -0,0 +1,12 @@
+// T.C = O(n)
+// S.C = O(n)
+vector<vector<int> > stockBuySell(vector<int> A, int n){
+    vector<vector<int>> ans;
+    
+    for (int i=0; i<n-1; i++){
+        if (A[i] < A[i+1]){
+            ans.push_back({i,i+1});
+        }
+    }
+    return ans;
+}
