3f8452bd35a21c36c109c56e1a4c2f0655aa3f37
Abhi Bhatt <109999045+Conan0122@users.noreply.github.com>
2024-09-24 00:55:53 +0530

    24 September 2024- Medium
    - Added another approach.

--- Find Missing And Repeating.cpp
+++ Find Missing And Repeating.cpp
@@ -32,5 +32,38 @@ public:
         return {repeat,missing};
     }
 };
-
+/////////////////////////////////////////////////////////////
 // Another efficient way of solving this problem is by marking every visited element with -ve such that in next traversal we would know which are the missing and repeated elements.
+// Approach 2: Marking visited element with -ve of it.
+// T.C = O(n)
+// S.C = O(1)
+// Logic:- Use indexes as elements and fill up every visited element with -ve of it.
+// For example:- [3, 1, 3]
+// iteration 1: [3, 1, -3]
+// iteration 2: [-3, 1, -3]
+// iteration 3: [-3, 1, -3], in this iteration we encounter -ve element so return that element as a repeating element cuz we know it is -ve since we already visited this element before.
+// Also, consider index 0 as element 1 and index 1 as element 2 and so on....
+// Now for finding missing element it's easy and simple just traverse the array again and look for element which is non -ve(not visited).
+// In our example case:- [-3,1,-3]
+// index 1 is still +ve so we know we didn't get element 2 in original array and hence this index isn't changed to -ve.
+vector<int> findTwoElement(vector<int>& arr) {
+    int n = arr.size();
+    int repeat=0, missing=0;
+    
+    // Find repeating
+    for (int i=0; i<n; i++){
+        if (arr[abs(arr[i])-1] < 0) repeat = abs(arr[i]);
+        else{
+            arr[abs(arr[i])-1] *= -1;
+        }
+    }
+    
+    // Find missing
+    for (int i=0; i<n; i++){
+        if (arr[i] > 0){
+            missing = i+1;
+            break;
+        }
+    }
+    return {repeat, missing};
+}
