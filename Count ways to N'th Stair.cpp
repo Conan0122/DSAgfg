18f1e011d39bb6af3d66d2c501ff669829a16269
Abhi Bhatt <109999045+Conan0122@users.noreply.github.com>
2025-01-13 02:05:51 +0530

    13 Jan 2025- Easy

---
+++ Count ways to N'th Stair.cpp
@@ -0,0 +1,17 @@
+long long countWays(int n) {
+    // Observation:
+    /*
+    n   res
+    1   1
+    2   2
+    3   2
+    4   3
+    5   3
+    6   4
+    7   4
+    8   5
+    9   5
+    10  6
+    */
+    return n/2+1;
+}
